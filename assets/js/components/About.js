c0a6be0a1a60ad059eec31b2c783188738217154
Robert Carrington <Robert.Carrington@q2ebanking.com>
2016-12-05 12:19:45 -0600

    Fixed link on About page and made font a little bigger

--- assets/js/components/About.js
+++ assets/js/components/About.js
@@ -4,12 +4,12 @@ var ReactDOM = require('react-dom');
 var About = React.createClass({
     render: function() {
         var linkStyle = { borderBottom: '1px dotted #000' }
-        var textStyle = { maxWidth: 400, minWidth: 300 }
-        var imgStyle = { width: 245, height: 245 }
+        var textStyle = { fontSize: 12, maxWidth: 400, minWidth: 300 }
+        var imgStyle = { width: 265, height: 265 }
         return (
             <div className="row">
                 <div className="col s10 m5" style={textStyle}>
-                    <span style={{fontWeight: 500}}>Hi, I'm Rob.</span> I like crunching data and building web apps with Python. I live in Austin, TX and do that flavor of work at <a style={linkStyle} href="http://www.q2ebanking.com">Q2&nbsp;eBanking</a>. On the side I hack on iOS and React.js <a style={linkStyle} href="/#/projects">projects.</a>
+                    <span style={{fontWeight: 500}}>Hi, I'm Rob.</span> I like crunching data and building web apps with Python. I live in Austin, TX and do that flavor of work at <a style={linkStyle} href="http://www.q2ebanking.com">Q2&nbsp;eBanking</a>. On the side I hack on iOS and React.js <a style={linkStyle} href="/projects">projects.</a>
                     <br/><br/>
                     <span style={{fontWeight: 500}}>Also excited about:</span><br/>
                     education technology <br/>
