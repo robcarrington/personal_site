336b959245e58782e83e64db51dd200e8722edd1
Robert Carrington <Robert.Carrington@q2ebanking.com>
2016-12-10 15:42:14 -0600

    Tweak tweak tweak

--- assets/js/components/About.js
+++ assets/js/components/About.js
@@ -5,7 +5,7 @@ var About = React.createClass({
     render: function() {
         var linkStyle = { borderBottom: '1px dotted #000' }
         var textStyle = { fontSize: 16, maxWidth: 400, minWidth: 300 }
-        var imgStyle = { width: 265, height: 265 }
+        var imgStyle = { maxWidth: 350, maxHeight: 350 }
         return (
             <div className="row left">
                 <div className="col s12 m12 l6" style={textStyle}>
