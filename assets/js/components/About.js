b91d6d3d2b516211072d88a2ee0cf69d9f51a97b
Robert Carrington <Robert.Carrington@q2ebanking.com>
2016-12-11 21:46:34 -0600

    Switching css layout for project page

--- assets/js/components/About.js
+++ assets/js/components/About.js
@@ -8,7 +8,7 @@ var About = React.createClass({
         var imgStyle = { maxWidth: 320, maxHeight: 320 }
         return (
             <div className="row">
-                <div className="col s12 m12 l6" style={textStyle}>
+                <div className="col s12 m6 l6" style={textStyle}>
                     <span style={{fontWeight: 500}}>Hi, I'm Rob.</span> I like crunching data and building web apps with Python. I live in Austin, TX and do that flavor of work at <a style={linkStyle} href="http://www.q2ebanking.com">Q2&nbsp;eBanking</a>. On the side I hack on iOS and React.js projects.
                     <br/><br/>
                     <span style={{fontWeight: 500}}>Also excited about:</span><br/>
@@ -18,7 +18,7 @@ var About = React.createClass({
                     comic strips <br/>
                     stoic philosophy <br/><br/>
                 </div>
-                <div style={{marginRight: 18}} className="col s12 m12 l6">
+                <div style={{marginRight: 18}} className="col s12 m6 l6">
                     <div className="card" style={imgStyle}>
                          <img style={imgStyle} src="/static/images/backyard.jpg" />
                     </div>
