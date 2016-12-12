d36a999ccd5d28704740c86c730e907066e15a90
Robert Carrington <Robert.Carrington@q2ebanking.com>
2016-12-11 21:24:25 -0600

    Mobile side nav tweak

--- assets/js/components/MobileSidebar.js
+++ assets/js/components/MobileSidebar.js
@@ -13,20 +13,20 @@ var MobileSidebar = React.createClass({
                 <a href="#" style={{margin: 9}} data-activates="mobile-menu" className="button-collapse">
                     <i className="material-icons">menu</i>
                 </a>
-                <ul className="side-nav" id="mobile-menu">
+                <ul className="side-nav right" id="mobile-menu">
                     <div>&nbsp;</div>
-                    <li><Link style={linkStyle} to='/about' >About</Link></li>
-                    <li><Link style={linkStyle} to='/blog' >Blog</Link></li>
-                    <li><Link style={linkStyle} to='/projects' >Projects</Link></li>
+                    <li><Link style={routeStyle} to='/about' >About</Link></li>
+                    <li><Link style={routeStyle} to='/blog' >Blog</Link></li>
+                    <li><Link style={routeStyle} to='/projects' >Projects</Link></li>
                     <div>&nbsp;</div>
-                    <a href='http://www.github.com/robcarrington'>&nbsp;
+                    <a style={linkStyle} href='http://www.github.com/robcarrington'>&nbsp;
                         <img src='/static/images/github.png' style={imgStyle}/> GitHub
                     </a>
-                    <a href='https://www.linkedin.com/in/robert-carrington-85073071'>&nbsp;
+                    <a style={linkStyle} href='https://www.linkedin.com/in/robert-carrington-85073071'>&nbsp;
                         <img src='/static/images/linkedin.png' style={imgStyle}/> LinkedIn
                     </a>
-                    <a style={{margin: 5}} href='https://www.dropbox.com/s/1sx23hcc0ox3ffx/rob_carrington_resume.pdf?dl=0'>
-                        <img src='/static/images/cv.png' style={imgStyle} /> Resume
+                    <a style={{margin: 5, fontSize: 18}} href='https://www.dropbox.com/s/1sx23hcc0ox3ffx/rob_carrington_resume.pdf?dl=0'>
+                        <img src='/static/images/cv.png' style={{margin: "10px 3px 0 3px", height: 20}} /> Resume
                     </a>
                 </ul>
             </div>
