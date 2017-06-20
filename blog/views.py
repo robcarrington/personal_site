3d6f66009fd904567d6686c691a8e5a9ae21c99c
Robert Carrington <Robert.Carrington@q2ebanking.com>
2017-06-19 21:03:42 -0500

    Trying to just swap out the Render to allow html

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer, StaticHTMLRenderer
 from blog.models import BlogPost
 from blog.serializers import BlogPostSerializer
 
 class JSONResponse(HttpResponse):
     def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
+        content = StaticHTMLRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
