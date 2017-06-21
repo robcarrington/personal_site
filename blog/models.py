35fe2c7b119c3ba27dffe4284ddb47514fb92c45
Robert Carrington <Robert.Carrington@q2ebanking.com>
2017-06-20 22:40:36 -0500

    Made link field not required

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ class BlogPost(models.Model):
     subtitle = models.CharField(max_length=100)
     body = models.TextField()
     color = models.CharField(default="#7a42f4", max_length=100)
-    link_url = models.CharField(max_length=100)
-    link_text = models.CharField(max_length=100)
+    link_url = models.CharField(max_length=100, blank=True)
+    link_text = models.CharField(max_length=100, blank=True)
 
     class Meta:
         ordering = ('created',)
