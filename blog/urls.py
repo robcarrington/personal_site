0aafc4eabe1ba368b7dc83acca9167c1f56d479a
Robert Carrington <Robert.Carrington@q2ebanking.com>
2017-09-04 11:53:01 -0500

    Changed routing logic to recognize alphanumeric urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from blog import views
 
 urlpatterns = [
     url(r'^list/$', views.blogpost_list),
-    url(r'^post/(?P<pk>[0-9]+)', views.blogpost_detail),
+    url(r'^post/(?P<pk>[a-zA-Z0-9-]+)', views.blogpost_detail),
 ]
