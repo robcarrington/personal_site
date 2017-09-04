f56baadd1111f7c91b4c301570dbccc2e967d8ac
Robert Carrington <Robert.Carrington@q2ebanking.com>
2017-09-04 11:47:12 -0500

    Changed primary key for blog posts. Restarted migrations folders

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class BlogPost(models.Model):
+    url = models.URLField(primary_key=True)
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=100)
